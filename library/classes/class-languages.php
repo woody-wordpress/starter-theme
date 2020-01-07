9736672f39846c04692dd591d74c4e172c02ba0d
Thomas Navarro <thomas.navarro@raccourci.fr>
2020-01-07 09:48:32 +0100

    add class languages

---
+++ library/classes/class-languages.php
@@ -0,0 +1,11 @@
+<?php
+
+class SubWoodyTheme_Languages
+{
+    public function languagesCustomization()
+    {
+        $return['flags'] = 'large';
+        // $return['hide_langs'] = ['en']; // Locale ['fr', 'en', 'es', 'nl', ...]
+        return $return;
+    }
+}
