aab67657d8123ca99c81658a81403683e2cf4226
Benoit Bouchaud <benoit.bouchaud@raccourci.fr>
2021-01-27 18:12:19 +0100

    up

--- library/hooks/class-enqueue-assets.php
+++ library/hooks/class-enqueue-assets.php
@@ -22,6 +22,21 @@ class SubWoodyTheme_Enqueue_Assets
 
     public function enqueueLibraries()
     {
+        //FIXME: Remove this lines if not a roadbook
+        $print_rdbk = get_query_var('print_rdbk');
+        if (!empty($roadbook) && !empty($print_rdbk)) {
+            wp_enqueue_style('addon-roadbook-print-stylesheet', WP_DIST_URL . '/css/rdbk_print.css', [], '', 'all');
+        }
+        // FIXME: add this lines to gulp.config.json if there is a roadbook
+        // "css": {
+        //     "entry": [
+        //         "main.scss",
+        //         "tourism.scss",
+        //         "admin.scss",
+        //         "print.scss",
+        //         "rdbk_print.scss"
+        //     ]
+        // }
     }
 
     public function setGlobalScriptString($globalScriptString)
