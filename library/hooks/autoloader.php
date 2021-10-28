30e33da699ddade1ca6d148f6ce4a03f07beea92
orpheebesson <bessonorphee@gmail.com>
2021-10-28 18:03:46 +0200

    [func] add class-hawwwai for sheet prefixes

--- library/hooks/autoloader.php
+++ library/hooks/autoloader.php
@@ -1,12 +1,13 @@
 <?php
 
-// TODO : Décommenter cette ligne après avoir créé les langues dans le backoffice
+// TODO: Décommenter cette ligne après avoir créé les langues dans le backoffice
 // define('WOODY_GENERATE_MENU', true);
 
 // new SubWoodyTheme_ACF();
 // new SubWoodyTheme_Admin();
 // new SubWoodyTheme_Context();
 new SubWoodyTheme_Enqueue_Assets();
+// new SubWoodyTheme_Hawwwai(); // TODO: Décommenter cette ligne en ajoutant vos préfixes de fiches SIT si besoin
 // new SubWoodyTheme_InclusionsOverride();
 // new SubWoodyTheme_Main();
 new SubWoodyTheme_Polylang();
---
+++ library/hooks/class-hawwwai.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Hawwwai
+ *
+ * @package SubWoodyTheme
+ * @since SubWoodyTheme 1.0.0
+ */
+
+class SubWoodyTheme_Hawwwai
+{
+    public function __construct()
+    {
+        $this->registerHooks();
+    }
+
+    protected function registerHooks()
+    {
+        add_filter('woody_hawwwai_aspects_list', [$this, 'woodyHawwwaiAspectsList']);
+    }
+
+    public function woodyHawwwaiAspectsList($aspects)
+    {
+        $custom_aspects = [
+            'your_aspect'
+        ];
+
+        $aspects = array_merge($custom_aspects, $aspects);
+
+        return $aspects;
+    }
+}
