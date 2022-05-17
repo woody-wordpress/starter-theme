ad45895321a2439ee1b365c5080b6e1304c21c9e
Dayraut Baptiste <baptiste.dayraut@icloud.com>
2022-05-17 10:59:57 +0200

    [func] New accessibility mode on nurtik form

---
+++ src/js/custom/filters.js
@@ -0,0 +1,6 @@
+import WoodyFilter from 'woody-library/assets/js/filter';
+
+// Accessibility mode on form nurtik
+// WoodyFilter.add('nurtik_accessibily_mode', function () {
+//   return true;
+// });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 // Import JS
+import './custom/filters';
 import 'woody-theme/src/js/front/_index';
 
 // Import Custom JS
