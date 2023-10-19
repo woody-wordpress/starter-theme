4c34daf69529b63481620d82bb61fafa71068555
Orphée <orphee.besson@raccourci.fr>
2023-10-19 10:09:55 +0200

    [func] instanciate class SubWoodyTheme_Main in starter theme

--- library/hooks/autoloader.php
+++ library/hooks/autoloader.php
@@ -10,6 +10,6 @@ new SubWoodyTheme_Context();
 new SubWoodyTheme_Enqueue_Assets();
 // new SubWoodyTheme_Hawwwai(); // TODO: Décommenter cette ligne en ajoutant vos préfixes de fiches SIT si besoin
 // new SubWoodyTheme_InclusionsOverride();
-// new SubWoodyTheme_Main();
+new SubWoodyTheme_Main();
 new SubWoodyTheme_Polylang();
 // new SubWoodyTheme_Tools();
