987c1546183fba2cd1aff58615543c3eb1d68fa7
Célia <celia.forgeron@raccourci.fr>
2025-01-30 12:19:16 +0100

    [MISC] Désactiver le js de calcul du padding du body en fonction de la taille du menu

--- src/js/custom/filters.js
+++ src/js/custom/filters.js
@@ -4,3 +4,8 @@ import WoodyFilter from 'woody-library/assets/js/filter';
 WoodyFilter.add('scroll_hide_menu_modifier', function() {
     return false;
 });
+
+// Disable padding calc in JS on the body
+WoodyFilter.add('padding_menu_calc_disable', function() {
+    return true;
+});
